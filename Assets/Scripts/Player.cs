2054fe49e1cd7dd40dc658de02fcf786065cc36c
mujeeb11503051 <mujeebmjrahman@gmail.com>
2022-01-02 20:38:16 +0100

    player movement added

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private BoxCollider2D boxCollider;
+
+    private Vector3 moveDelta;
+
+    private void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        //Reset MoveDelta
+        moveDelta = new Vector3(x, y, 0);
+
+        //swap sprite direction, weather u'r going right or left
+
+        if(moveDelta.x > 0)
+        {
+            transform.localScale = Vector3.one;
+        }
+        else if(moveDelta.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        //move
+        transform.Translate(moveDelta * Time.deltaTime);
+
+
+    }
+
+}
