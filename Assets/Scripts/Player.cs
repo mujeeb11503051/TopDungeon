6c883f3386983dae2ff8e340020b68e080e226dd
mujeeb11503051 <mujeebmjrahman@gmail.com>
2022-01-03 00:12:26 +0100

    added a basic player - npc collision and player - wall collision

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private BoxCollider2D boxCollider;
-
     private Vector3 moveDelta;
+    private RaycastHit2D hit;
 
     private void Start()
     {
@@ -32,8 +32,23 @@ public class Player : MonoBehaviour
             transform.localScale = new Vector3(-1, 1, 1);
         }
 
-        //move
-        transform.Translate(moveDelta * Time.deltaTime);
+        //make sure we can move in this direction, by casting a box there first, if the box returns null, we are free to move
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+
+        if(hit.collider == null)
+        {
+            //move
+            transform.Translate(0,moveDelta.y * Time.deltaTime, 0);
+        }
+
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(moveDelta.x, 0), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+
+        if (hit.collider == null)
+        {
+            //move
+            transform.Translate( moveDelta.x * Time.deltaTime, 0, 0);
+        }
+
 
 
     }
